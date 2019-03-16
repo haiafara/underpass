e0e905da50ced7d8011a120b0d4b495346a97f99
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-03-16 19:16:06 +0200

    parser_spec now covers all the parser

--- spec/support/nodes_and_ways.rb
+++ spec/support/nodes_and_ways.rb
@@ -40,4 +40,42 @@ module NodesAndWays
         3
       ]
     }.freeze
+
+  NODES_AND_WAYS =
+    [
+      {
+        type: 'way',
+        id: 1,
+        nodes: [2, 3, 4, 5],
+        tags: {
+          amenity: 'something',
+          building: 'yes',
+          name: 'Test'
+        }
+      },
+      {
+        type: 'node',
+        id: 2,
+        lat: -1,
+        lon: 1
+      },
+      {
+        type: 'node',
+        id: 3,
+        lat: 1,
+        lon: 1
+      },
+      {
+        type: 'node',
+        id: 4,
+        lat: 1,
+        lon: -1
+      },
+      {
+        type: 'node',
+        id: 5,
+        lat: -1,
+        lon: -1
+      }
+    ].freeze
 end
--- spec/underpass/ql/parser_spec.rb
+++ spec/underpass/ql/parser_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'support/nodes_and_ways'
 require 'underpass'
 
 describe Underpass::QL::Parser do
@@ -20,7 +21,7 @@ describe Underpass::QL::Parser do
       allow(response_double).to receive(:body)
     end
     it 'calls JSON.parse and the extractor methods and returns the instance' do
-      expect(JSON).to receive(:parse).once.and_return({ elements: 'test' })
+      expect(JSON).to receive(:parse).once.and_return(elements: 'test')
       expect(instance).to receive(:extract_indexed_nodes).once
       expect(instance).to receive(:extract_indexed_ways).once
       expect(instance.parse).to eq(instance)
@@ -29,11 +30,48 @@ describe Underpass::QL::Parser do
 
   describe '#matches' do
     before do
-      instance.instance_variable_set(:@ways, {a: 1, b: 2})
+      instance.instance_variable_set(:@ways, a: 1, b: 2)
     end
     it 'calls polygon from way and returns matches' do
       expect(Underpass::QL::Shape).to receive(:polygon_from_way).twice.and_return('test')
       expect(instance.matches.size).to eq(2)
+    end
+  end
+
+  describe '#extract_indexed_nodes' do
+    let(:nodes_and_ways) { NodesAndWays::NODES_AND_WAYS }
+    it 'returns a hash of nodes indexed by id' do
+      result = instance.send(:extract_indexed_nodes, nodes_and_ways)
+      expect(result.size).to eq(4)
+      expect(result[5]).to eq(
+        type: 'node',
+        id: 5,
+        lat: -1,
+        lon: -1
+      )
+    end
+  end
+
+  describe '#extract_indexed_ways' do
+    let(:nodes_and_ways) { NodesAndWays::NODES_AND_WAYS }
+    it 'returns a hash of ways indexed by id' do
+      result = instance.send(:extract_indexed_ways, nodes_and_ways)
+      expect(result.size).to eq(1)
+      expect(result.first).to eq(
+        [
+          1,
+          {
+            type: 'way',
+            id: 1,
+            nodes: [2, 3, 4, 5],
+            tags: {
+              amenity: 'something',
+              building: 'yes',
+              name: 'Test'
+            }
+          }
+        ]
+      )
     end
   end
 end
